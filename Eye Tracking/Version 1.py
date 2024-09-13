2e7bf3d9cd0b9d63bbe276e94ecba9df6d3fad0f
Alysandraa <165456747+Alysandraa@users.noreply.github.com>
2024-09-14 09:09:18 +1000

    Update Version 1.py

--- Eye Tracking/Version 1.py
+++ Eye Tracking/Version 1.py
@@ -1,35 +1,43 @@
-import os, sys, inspect
 import cv2
 import numpy as np
-import math
-import serial
-import time
+import matplotlib.pyplot as plt
 
+face_data = ('haarcascade_frontalface_default.xml')
+eye_data = ('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier(face_data)
+eye_cascade = cv2.CascadeClassifier(eye_data)
 
-def frame_prep(frame):
-    #put frame in black and white
-    bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #blur the black and white frame
-    blur = cv2.GaussianBlur(bw, (9, 9), 0)
-    #detect edges on the product of the previous 2 steps
-    edges = cv2.Canny(blur, 50, 150)
-    cv2.imshow("Edges",edges)
-    return edges
-
-cam = cv2.VideoCapture(0)
-cam.set(cv2.CAP_PROP_FRAME_WIDTH,320)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
-
-ser = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
+cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cam.read()
-    frame = cv2.flip(frame,-1)
-    height, width, _ = frame.shape
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    key = cv2.waitKey(1)        
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        print("found face")
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+            eye_roi_gray = roi_gray[ey:ey+eh, ex:ex+ew]
+            eye_roi_color = roi_color[ey:ey+eh, ex:ex+ew]
+            _, eye_thresh = cv2.threshold(eye_roi_gray, 50, 255, cv2.THRESH_BINARY_INV)
+            contours, _ = cv2.findContours(eye_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            print("found eyes")
+            if len(contours) > 0:
+                pupil = max(contours, key = cv2.contourArea)
+                x1, y1, w1, h1 = cv2.boundingRect(pupil)
+                center = (int(x1 + w1/2), int(y1 + h1/2))
+                cv2.circle(eye_roi_color, center, 3, (255, 0, 0), -1)
+                cv2.imshow('Eye Tracking', eye_roi_color)
+                print("found pupils")
+    cv2.imshow('Eye Tracking', gray)
+    print("showing image")            
+    key = cv2.waitKey(1)
     if key != -1:
         break
 
-cam.release()
+cap.release()
 cv2.destroyAllWindows()

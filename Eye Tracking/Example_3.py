9bb275e963ee85ad9d7e375613c6bfa6b3666d77
Alysandraa <165456747+Alysandraa@users.noreply.github.com>
2024-09-16 11:46:16 +1000

    Create Example_3.py

---
+++ Eye Tracking/Example_3.py
@@ -0,0 +1,96 @@
+import cv2
+import numpy as np
+
+
+# init part
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+detector_params = cv2.SimpleBlobDetector_Params()
+detector_params.filterByArea = True
+detector_params.maxArea = 1500
+detector = cv2.SimpleBlobDetector_create(detector_params)
+
+
+def detect_faces(img, cascade):
+    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    coords = cascade.detectMultiScale(gray_frame, 1.3, 5)
+    #what is this doing?
+    if len(coords) > 1:
+        biggest = (0, 0, 0, 0)
+        for i in coords:
+            if i[3] > biggest[3]:
+                biggest = i
+        biggest = np.array([i], np.int32)
+    elif len(coords) == 1:
+        biggest = coords
+    else:
+        return None#do i need this?
+    for (x, y, w, h) in biggest:
+        frame = img[y:y + h, x:x + w]
+    return frame
+
+
+def detect_eyes(img, cascade):
+    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    eyes = cascade.detectMultiScale(gray_frame, 1.3, 5)  # detect eyes
+    width = np.size(img, 1)  # get face frame width
+    height = np.size(img, 0)  # get face frame height
+    for (x, y, w, h) in eyes:
+        if y > height / 2:
+            pass
+        eyecenter = x + w / 2  # get the eye center
+        if eyecenter < width * 0.5:
+            left_eye = img[y:y + h, x:x + w]
+        else:
+            right_eye = img[y:y + h, x:x + w]
+    return left_eye, right_eye
+
+
+def cut_eyebrows(img):
+    height, width = img.shape[:2]
+    eyebrow_h = int(height / 4)
+    img = img[eyebrow_h:height, 0:width]  # cut eyebrows out (15 px)
+
+    return img
+
+
+def blob_process(img, threshold, detector):
+    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, img = cv2.threshold(gray_frame, threshold, 255, cv2.THRESH_BINARY)
+    img = cv2.erode(img, None, iterations=2)#idea is exactly like soil erosion. erodes away boundaries of foreground object
+    img = cv2.dilate(img, None, iterations=4)#commonly used with erosion (for noise removal in images) because erode also shrinks the object so this makes it larger again 
+    img = cv2.medianBlur(img, 5)#takes the median values of each pixel and replaces the central element with this value
+    keypoints = detector.detect(img)#blobl detection look at example on laptop
+    print(keypoints)
+    return keypoints
+
+
+def nothing(x):#whats the point in doing this?
+    pass
+
+
+def main():
+    #cap = cv2.VideoCapture(0)
+    cv2.namedWindow('image')
+    cv2.createTrackbar('threshold', 'image', 0, 255, nothing)
+    while True:
+        #_, frame = cap.read()
+        frame  = cv2.imread("face.jpg")
+        face_frame = detect_faces(frame, face_cascade)
+        if face_frame is not None:
+            eyes = detect_eyes(face_frame, eye_cascade)
+            for eye in eyes:
+                if eye is not None:
+                    threshold = r = cv2.getTrackbarPos('threshold', 'image')
+                    eye = cut_eyebrows(eye)
+                    keypoints = blob_process(eye, threshold, detector)
+                    eye = cv2.drawKeypoints(eye, keypoints, eye, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        cv2.imshow('image', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    #cap.release()
+    #cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
